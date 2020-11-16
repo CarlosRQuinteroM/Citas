c24bb05bc0f9bf94315705c12eca90da6b93f52d
CarlosRQuinteroM <carlosquintero92.cq@gmail.com>
2020-11-16 23:37:41 +0100

    1...Validación de Formularios en React

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -10,6 +10,7 @@ const Formulario  = () => {
          hora: '', 
          sintomas: '',
      });
+      const [error, actualizarError ] = useState(false)
 
 
      // Funcion que se ejecuta cada que el usuario escribe en un input
@@ -19,15 +20,40 @@ const Formulario  = () => {
             [e.target.name]:  e.target.value
         })
        }
-     
+
        //Extraer los valores
        const {mascota, propietario, fecha, hora, sintomas } = cita;
+
+       //Cuando el usuario preciona agregar cita 
+       const submitCita = e => {
+           e.preventDefault();
+            
+           
+          //validar
+        if(mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || hora.trim() === '' || sintomas.trim() === ''){
+            actualizarError(true);
+            return;
+        }
+
+
+          //asignar un ID
+
+          //crear la cita 
+          
+          //reiniciar el form
+       }
+
 
       return ( 
         <Fragment>
         <h2> Crear Cita</h2>
 
-        <form>
+        {error? <p className="alerta-error">Todos los campos son obligatorios</p>  : null}
+
+        <form
+        onSubmit={submitCita}
+        >
+
       <label> Nombre Mascota </label>
       <input 
            type="text"
