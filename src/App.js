328463282d652f8fb25f6dcaa25d04b3d20c5e40
CarlosRQuinteroM <carlosquintero92.cq@gmail.com>
2020-11-17 12:50:03 +0100

    1.. Mostrando un mensaje de forma condicional

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
        const nuevasCitas= citas.filter(cita => cita.id !== id)
        guardarCitas(nuevasCitas)
      }
+     // mensaje condicional 
+     const titulo = citas.length === 0 ?  'No hay citas'  : 'Administra tus citas'
   return (
     <Fragment>
       <h1>Administrador de Pacientes </h1>
@@ -29,7 +31,7 @@ function App() {
             />
          </div>
          <div className="one-half column">
-            <h2>Administra tus citas</h2>
+              <h2>{titulo}</h2>
             {citas.map(cita => (
               <Cita
               key={cita.id}
